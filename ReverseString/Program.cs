3a7ed1900d13faa505a601494fad0f36fef2586b
smruti swain <smrutiswain35@outlook.com>
2020-10-27 19:56:46 +0530

    second commit

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -6,8 +6,17 @@ namespace ReverseString
     {
         static void Main(string[] args)
         {
+            string _inputstring = string.Empty;
+            string _ReverseString = string.Empty;
+            Console.WriteLine("Enter the string");
+            _inputstring = Console.ReadLine();
+            for(int i= _inputstring.Length-1; i >=0; i--)
+            {
+                _ReverseString += _inputstring[i];
 
-            Console.WriteLine("Hello World!");
+            }
+            Console.WriteLine("Reverse String is:{0}", _ReverseString);
+            Console.ReadLine();
         }
     }
 }
